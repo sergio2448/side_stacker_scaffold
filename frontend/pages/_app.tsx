8caa10fc757fef8c173a1525ec58f039541b02ad
Igor Loskutov <igor.loskutoff@gmail.com>
2022-05-19 21:44:54 +0700

    feat: isBrowser "tool" for nextjs

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,9 @@ import "../styles/globals.css";
 import "../styles/All.css";
 import type { AppProps } from "next/app";
 
+// may come in handy i.e. in case you'll want to set up an apollo client subscription etc
+const isBrowser = typeof window !== "undefined";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
